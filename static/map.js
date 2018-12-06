8c0b0ff35a64c40dc68d3548739c99f544ff6988
Max Carey <maxcarey@protonmail.com>
2018-12-06 13:16:17 -0600

    Clean up print statements and other code

--- static/map.js
+++ static/map.js
@@ -34,21 +34,10 @@ for (var i = 0; i < arrayLength; i++) {
 }
 
 
-/*  Get the destination data*/
-// No need for AJAX call because of CORS, when developing locally, need to call another file
-// Call the data
-//var myDestinations = [];
-//$.ajax({
-//    url: 'https://www.totago.co/api/v1/destinations.json?region_id=1',
-//    async: false,
-//    dataType: 'json',
-//    success: function (json) {
-//        myDestinations = json;
-//    }
-//});
 
 
 // Pull the array of destination objects out of the JSON variable
+// This will have to become an AJAX call before deployment
 var waDestinations = destinationsWA['destinations'];
 
 
@@ -83,9 +72,6 @@ var createGeoJSONCircle = function(center, radiusInKm, points) {
     }
     ret.push(ret[0]);
 
-    console.log("internal function coordinates");
-    console.log(ret);
-
     return {
         "type": "geojson",
         "data": {
@@ -113,9 +99,7 @@ var ViewModel = function() {
     
         // https://stackoverflow.com/a/7178381/5420796
         for(var i = 0; i < waData.length; i += 1) {
-            //console.log(waData[i]["ZCTA5CE10"]);
             if(waData[i]["ZCTA5CE10"] == self.highlightedPostalCode()) {
-                //console.log(waData[i]["postalCodeHits"]);
                 return waData[i]["postalCodeHits"];
             }
         }   
@@ -127,27 +111,18 @@ var ViewModel = function() {
 
 
     var waDestinationsLength = waDestinations.length;
-    //console.log(waDestinationsLength);
-    //console.log(waDestinationsLength);
+    
     for (var i = 0; i < waDestinationsLength; i++) {
     
         var name = waDestinations[i]['name'];
         var lat = parseFloat(waDestinations[i]['begin_lat']);
         var lng = parseFloat(waDestinations[i]['begin_lng']);
 
-        //console.log("Check me: ")
-        //console.log(lat);
-        //console.log(lng);
-
         latLngArray = [];
         latLngArray.push(lat);
         latLngArray.push(lng);
-        
-        console.log("Before function call")
-        console.log(latLngArray);
 
         geoJSONCircle = createGeoJSONCircle(latLngArray, 1);
-        console.log(geoJSONCircle);
         
         destinationObject = {
             name: name,
@@ -192,9 +167,7 @@ $(document).ready(function () {
         // Calulate Max Value
         // https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects
         maxValue = Math.max.apply(Math, waData.map(function(o) { return o.postalCodeHits; }))
-        
-        //console.log("Max value")
-        //console.log(maxValue)
+
 
         // Calculate color for each state based on the unemployment rate
         waData.forEach(function(row) {
@@ -223,8 +196,6 @@ $(document).ready(function () {
             var name = my.viewModel.destinationCircles()[i].name;
             var circle = my.viewModel.destinationCircles()[i].geoJSONCircle;
             
-            console.log(name);
-            console.log(circle);
 
             map.addSource(name,circle);
             map.addLayer({
@@ -233,58 +204,14 @@ $(document).ready(function () {
                 "source": name,
                 "layout": {},
                 "paint": {
-                    "fill-color": "blue",
+                    "fill-color": "red",
                     "fill-opacity": 1.0
                 }
             });
 
             var mapLayer = map.getLayer(name);
 
-            // This code esentially if the layer is undefined when it's added
-            if(typeof mapLayer == 'undefined') {
-            // Remove map layer & source.
-            console.log("UNDEFINED LAYER");
-            }
-
         }
-
-        circle = createGeoJSONCircle([47.708013, -122.335167], 1)
-        console.log("Circle should have to be like this: ")
-        console.log(circle);
-        map.addSource("Mount Si", circle);
-
-        map.addLayer({
-            "id": "Mount Si",
-            "type": "fill",
-            "source": "Mount Si",
-            "layout": {},
-            "paint": {
-                "fill-color": "red",
-                "fill-opacity": 1.0
-            }
-        });
-
-
-        // TODO: Add legend from tutorial
-        // As of this point this part was just copied over
-        // https://www.mapbox.com/help/choropleth-studio-gl-pt-2/#mission-complete
-        //var layers = ['0-10', '10-20', '20-50', '50-100', '100-200', '200-500', '500-1000', '1000+'];
-        //var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
-
-        //for (i = 0; i < layers.length; i++) {
-        //   var layer = layers[i];
-        //    var color = colors[i];
-        //    var item = document.createElement('div');
-        //    var key = document.createElement('span');
-        //    key.className = 'legend-key';
-        //    key.style.backgroundColor = color;
-
-        //    var value = document.createElement('span');
-        //    value.innerHTML = layer;
-        //    item.appendChild(key);
-        //    item.appendChild(value);
-        //    legend.appendChild(item);
-        //}
 
         map.on('mousemove', function (e) {
         var features = map.queryRenderedFeatures(e.point);
@@ -297,9 +224,7 @@ $(document).ready(function () {
 });
 
 /** Apply Bindings */
-// This cod was also just copied over from what I had on knockout from
-// My Udacity project
-// https://github.com/axme100/mapProj/blob/master/js/app.js
+// This was copied over from my udacity project: https://github.com/axme100/mapProj/blob/master/js/app.js
 // I'm creating an instance of my view model called "my" 
 // The idea for this comes from the following post on the next line:
 // https://stackoverflow.com/questions/46943988/how-can-i-access-an-observable-outside-the-viewmodel-in-knockoutjs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
